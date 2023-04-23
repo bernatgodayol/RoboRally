60492f26e6a4ce00a633554894f715ecb9cb4cab
mathieu <mathieu.jensen@hotmail.fr>
2023-04-23 09:36:22 +0200

    Boards

--- Cucmber_Project/src/main/java/model/LeftTurn.java
+++ Cucmber_Project/src/main/java/model/LeftTurn.java
@@ -7,13 +7,13 @@ public class LeftTurn implements Card {
 		if(robot.getDirection()==Direction.NORTH) {
 			robot.setDirection(Direction.WEST);
 		}
-		if(robot.getDirection()==Direction.WEST) {
+		else if(robot.getDirection()==Direction.WEST) {
 			robot.setDirection(Direction.SOUTH);
 		}
-		if(robot.getDirection()==Direction.SOUTH) {
+		else if(robot.getDirection()==Direction.SOUTH) {
 			robot.setDirection(Direction.EAST);
 		}
-		if(robot.getDirection()==Direction.EAST) {
+		else if(robot.getDirection()==Direction.EAST) {
 			robot.setDirection(Direction.NORTH);
 		}
 		
--- Cucmber_Project/src/main/java/model/UTurn.java
+++ Cucmber_Project/src/main/java/model/UTurn.java
@@ -7,13 +7,13 @@ public class UTurn implements Card {
 		if(robot.getDirection()==Direction.NORTH) {
 			robot.setDirection(Direction.SOUTH);
 		}
-		if(robot.getDirection()==Direction.WEST) {
+		else if(robot.getDirection()==Direction.WEST) {
 			robot.setDirection(Direction.EAST);
 		}
-		if(robot.getDirection()==Direction.SOUTH) {
+		else if(robot.getDirection()==Direction.SOUTH) {
 			robot.setDirection(Direction.NORTH);
 		}
-		if(robot.getDirection()==Direction.EAST) {
+		else if(robot.getDirection()==Direction.EAST) {
 			robot.setDirection(Direction.WEST);
 		}
 
